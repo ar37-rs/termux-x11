37d4433137d0730bd2d72b958ad6c0e49020a1db
Adya Roby <adyaro37@gmail.com>
2024-12-24 22:16:28 +0700

    Update renderer.c

--- app/src/main/cpp/lorie/renderer.c
+++ app/src/main/cpp/lorie/renderer.c
@@ -236,7 +236,7 @@ int renderer_init(JNIEnv* env, int* legacy_drawing, uint8_t* flip) {
                 .height = 64,
                 .layers = 1,
                 .usage = AHARDWAREBUFFER_USAGE_GPU_SAMPLED_IMAGE | AHARDWAREBUFFER_USAGE_CPU_WRITE_OFTEN | AHARDWAREBUFFER_USAGE_CPU_READ_OFTEN,
-                .format = AHARDWAREBUFFER_FORMAT_B8G8R8A8_UNORM
+                .format = AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM
         };
 
         status = AHardwareBuffer_allocate(&d0, &new);
