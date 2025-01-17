e386bd6f18674a2d90fdcc5808fd60baffc9d5ee
Adya Roby <adyaro37@gmail.com>
2025-01-18 00:05:50 +0700

    Update CmdEntryPoint.java

--- app/src/main/java/com/termux/x11/CmdEntryPoint.java
+++ app/src/main/java/com/termux/x11/CmdEntryPoint.java
@@ -183,7 +183,25 @@ public class CmdEntryPoint extends ICmdEntryInterface.Stub {
         }
         handler = new Handler();
         ctx = createContext();        
-        System.loadLibrary("Xlorie");
+        String path = "lib/" + Build.SUPPORTED_ABIS[0] + "/libXlorie.so";
+        ClassLoader loader = CmdEntryPoint.class.getClassLoader();
+        URL res = loader != null ? loader.getResource(path) : null;
+        String libPath = res != null ? res.getFile().replace("file:", "") : null;
+        if (libPath != null) {
+            try {
+                System.load(libPath);
+            } catch (Exception e) {
+                Log.e("CmdEntryPoint", "Failed to dlopen " + libPath, e);
+                System.err.println("Failed to load native library. Did you install the right apk? Try the universal one.");
+                System.exit(134);
+            }
+        } else {
+            // It is critical only when it is not running in Android application process
+            if (MainActivity.getInstance() == null) {
+                System.err.println("Failed to acquire native library. Did you install the right apk? Try the universal one.");
+                System.exit(134);
+            }
+        }
         System.loadLibrary("EGL_angle");
         System.loadLibrary("GLESv2_angle");
         System.loadLibrary("GLESv1_CM_angle");
